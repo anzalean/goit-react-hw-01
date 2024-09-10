7c5a829b92a32dd56cb9bb4666faf04ce859f0ef
anzalean <zablovskaya04@gmail.com>
2024-09-10 20:42:52 +0300

    add 3 tasks

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+import Profile from './components/Profile/Profile'
+import userData from "./userData.json";
+import friends from "./friends.json";
+import FriendList from './components/FriendList/FriendList';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+import transactions from './transactions.json';
+
+
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        avatar={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+       <TransactionHistory items={transactions} />
+    </>
+  );
+};
+export default App
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,30 @@
+
+import PropTypes from 'prop-types';
+import FriendListItem from '../FriendListItem/FriendListItem';
+import styles from './FriendList.module.css';
+
+const FriendList = ({ friends }) => {
+    return (
+        <ul className={styles.friendList}>
+            {friends.map(friend => (
+                <li key={friend.id}>
+                    <FriendListItem 
+                        avatar={friend.avatar}
+                        name={friend.name}
+                        isOnline={friend.isOnline}
+                    />
+                </li>
+            ))}
+        </ul>
+    );
+};
+
+FriendList.propTypes = {
+    friends: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+        }),
+    ).isRequired,
+};
+
+export default FriendList;
